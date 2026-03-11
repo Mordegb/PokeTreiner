d95ef445fb7758315f7cd4dfece4c21bdfbfbf52
Cadu <cadumalmeida13@gmail.com>
2026-03-11 19:33:18 -0300

    Style: botão de mostrar senha e responsividade ajustada

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router , RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './login.scss',
 })
 export class Login {
+  private router = inject(Router);
+
+  HowIsThatPokemon(){
+    window.open('https://www.youtube.com/watch?v=WSGV_n6H1n0' , '_blank' )
+  }
+
+  InputPassword:String = 'password'
+  ShowPassword(){
+    this.InputPassword = this.InputPassword === 'password' ? 'text':'password'
+  }
 
 }
