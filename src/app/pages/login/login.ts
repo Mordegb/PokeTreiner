cd0da64e38aafd23008b3498639aeb70bfa64669
Cadu <cadumalmeida13@gmail.com>
2026-03-12 17:37:48 -0300

    logica pronta

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,5 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { Router , RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import {
+  FormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +17,49 @@ import { Router , RouterLink } from '@angular/router';
 export class Login {
   private router = inject(Router);
 
-  HowIsThatPokemon(){
-    window.open('https://www.youtube.com/watch?v=WSGV_n6H1n0' , '_blank' )
+  UsuariosExemplo = [
+    { email: 'joao.silva@gmail.com', senha: 'joao123' },
+    { email: 'maria.santos@gmail.com', senha: 'maria456' },
+    { email: 'pedro.oliveira@gmail.com', senha: 'pedro789' },
+    { email: 'ana.costa@gmail.com', senha: 'ana321' },
+    { email: 'carlos.almeida@gmail.com', senha: 'cadu123' },
+    { email: 'julia.pereira@gmail.com', senha: 'julia987' },
+    { email: 'lucas.rodrigues@gmail.com', senha: 'lucas147' },
+    { email: 'mariana.alves@gmail.com', senha: 'mariana258' },
+    { email: 'admin@gmail.com', senha: 'adm1234@' },
+    { email: 'fernanda.gomes@gmail.com', senha: 'fernanda741' },
+  ];
+
+  HowIsThatPokemon() {
+    window.open('https://www.youtube.com/watch?v=WSGV_n6H1n0', '_blank');
   }
 
-  InputPassword:String = 'password'
-  ShowPassword(){
-    this.InputPassword = this.InputPassword === 'password' ? 'text':'password'
+  InputPassword: String = 'password';
+  ShowPassword() {
+    this.InputPassword = this.InputPassword === 'password' ? 'text' : 'password';
   }
 
+   LoginForm = new FormGroup({
+    UserEmail: new FormControl('',[Validators.required, Validators.email]),
+    UserPassword: new FormControl('',[
+      Validators.required,
+      Validators.minLength(4),
+      Validators.pattern(/[!@#$&_-]/),
+    ]),
+  });
+
+  BUscarUsuario() {
+    const EmailDigitado = this.LoginForm.value.UserEmail;
+    const SenhaDigitada = this.LoginForm.value.UserPassword;
+
+    const foundUser = this.UsuariosExemplo.find((u) => u.email === EmailDigitado);
+    if(foundUser && foundUser.email === EmailDigitado){
+      // this.router.navigate(['/home'])
+      console.log(this.LoginForm)
+    }
+    else{
+      this.LoginForm.reset
+      console.log('nun deu bom')
+    }
+  }
 }
