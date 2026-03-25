fca0297a81ccb75020fa6227653f6556d9d7cbf6
Cadu <cadumalmeida13@gmail.com>
2026-03-25 00:04:56 -0300

    fix:error button active

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 import {
   FormsModule,
   FormControl,
@@ -9,11 +10,10 @@ import {
   ɵInternalFormsSharedModule,
 } from '@angular/forms';
 import { UserService } from '../../services/Register/user.service';
-import { error } from 'console';
 
 @Component({
   selector: 'app-register',
-  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule,FormsModule],
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule, FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
@@ -22,14 +22,13 @@ export class Register {
   private userService = inject(UserService);
 
   RegisterForm = new FormGroup({
-      UserName: new FormControl('', [
+    UserName: new FormControl('', [
       Validators.required,
-      Validators.minLength(5),
+      Validators.minLength(4),
       Validators.maxLength(25),
     ]),
-    UserEmail: new FormControl('', Validators.required),
+    UserEmail: new FormControl('', [Validators.required, Validators.email]),
     UserPassword: new FormControl('', [Validators.required, Validators.minLength(5)]),
-  
   });
 
   teste() {
@@ -48,17 +47,19 @@ export class Register {
     const senha = this.RegisterForm.value.UserPassword ?? '';
     const name = this.RegisterForm.value.UserName ?? '';
 
-    this.userService.CriarConta(name,email, senha).subscribe({
+    this.userService.CriarConta(name, email, senha).subscribe({
       next: (response) => {
-        if (response.error === false) {
+        if (!response.error) {
           console.log('usuario criado, manda o dan conferir');
-          //navigate pro login
-          //mensagem de que criar a conta deu certo
+          this.router.navigate(['/login'])
+          // notficação de coisa boa
         }
       },
-      error: (error) => {
-        console.log('não funcionou e não criou');
-        //mensagem com o motivo de dar erado
+      error: (error: HttpErrorResponse) => {
+        console.error('erro no logi:',error)
+        if (error.status === 400) {
+          console.log('esse email ja esta sendo usado.');
+        }
       },
     });
   }
